e3a3bdecdebe88dd62ea945c04e2a398b2978821
koder10001 <koder10001@gmail.com>
2026-08-03 13:17:04 -0500

    fix my stupidity

--- ip.h
+++ ip.h
@@ -6,10 +6,10 @@
 
 char* uintToBin(size_t num, uint8_t bitlen);
 char* uintToHex(size_t num, uint8_t len);
-// char* binToHex(char* bin,  size_t bit);
-// size_t binToUint(char* bin,  size_t bit);
-// char* hexToBin(char* hex,  size_t bit);
-// size_t hexToUint(char* hex, size_t bit);
+char* binToHex(char* bin,  size_t bit);
+size_t binToUint(char* bin,  size_t bit);
+char* hexToBin(char* hex,  size_t bit);
+size_t hexToUint(char* hex, size_t bit);
 
 
 int isLittleEndian();
